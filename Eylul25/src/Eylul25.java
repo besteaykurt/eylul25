f7f5504ab9c4aa653f349c596030174a99713d45
20090104019 <20090104019@muhlab11.lab.khas.edu.tr>
2012-09-25 08:19:36 -0430

    bir kenarı bulma işlemi eklendi

--- Eylul25/src/Eylul25.java
+++ Eylul25/src/Eylul25.java
@@ -6,9 +6,12 @@ public class Eylul25 {
 		
            int a=3;  
            int b=4;
+           int d=7;
+           int f=25;
            double c=Math.sqrt((a*a)+(b*b));
-           System.out.println("Kenarlarý " + a + " ve " + b + " olan üçgenin hipotenüsü "+ c);
-
+           double e=Math.sqrt((f*f)-(d*d));
+           System.out.println("Kenarlarý " + a + " ve " + b + " olan üçgenin hipotenüsü = "+ c);
+           System.out.println("Hipotenüsü " + f + " ve bir kenarý " + d + " olan üçgenin diðer kenarý = "+ e);
 	}
 
 }
