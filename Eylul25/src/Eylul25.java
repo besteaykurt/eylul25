c9f08241f7f8147cf612a5dae2725b2cc248f37c
20090104019 <20090104019@muhlab11.lab.khas.edu.tr>
2012-09-25 07:47:14 -0430

    üçgen karekök hesaplama yazıldı

---
+++ Eylul25/src/Eylul25.java
@@ -0,0 +1,14 @@
+
+public class Eylul25 {
+
+	
+	public static void main(String[] args) {
+		
+           int a=3;  
+           int b=4;
+           double c=Math.sqrt((a^2)+(b^2));
+           System.out.println("Kenarlarý " + a + " ve " + b + " olan üçgenin hipotenüsü "+ c);
+
+	}
+
+}
